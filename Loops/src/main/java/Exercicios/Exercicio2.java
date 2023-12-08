f5859b09bdb5cb865e2fe7e7465feefb59e14929
Giovanni Torelli <giovanni.walter@outlook.com>
2023-12-08 11:23:49 -0300

    Finished Exercise 2

---
+++ Loops/src/main/java/Exercicios/Exercicio2.java
@@ -0,0 +1,27 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num;
+        int pares = 0, impares = 0;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Digite o " + i + "ª número: ");
+            num = input.nextInt();
+            if(num % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+
+        System.out.println("Total de números pares: " + pares);
+        System.out.println("Total de números ímpares: " + impares);
+    }
+
+}
